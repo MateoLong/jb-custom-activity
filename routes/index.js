6dedf11c7d0320cce1650ebedf64637411d252b1
Mateo Long <mlongg@deloitte.com>
2024-07-23 11:34:12 -0300

    changes to routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
 /* POST to execute */
 router.post('/execute', async function(req, res, next) {
   var inArguments = req.body.inArguments;
