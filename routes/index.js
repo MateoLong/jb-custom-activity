82799798e56685eef66959214100a01258ee53c8
Mateo Long <mlongg@deloitte.com>
2024-07-30 08:50:05 -0300

    createCustomer change

--- routes/index.js
+++ routes/index.js
@@ -62,8 +62,7 @@ router.get('/getCompanyInfo', function (req, res) {
     });
 });
 
-router.get('/createCustomer', function (req, res, next) {
-  console.log('entra al createCustomer');
+router.get('/createCustomer', function (req, res) {
   const companyID = oauthClient.getToken().realmId;
 
   const url =
@@ -71,44 +70,18 @@ router.get('/createCustomer', function (req, res, next) {
       ? OAuthClient.environment.sandbox
       : OAuthClient.environment.production;
 
-  // Body sample from API explorer examples
-  const body = {
-  TrackQtyOnHand: true,
-  Name: 'Garden Supplies',
-  QtyOnHand: 10,
-  InvStartDate: '2015-01-01',
-  Type: 'Inventory',
-  IncomeAccountRef: {
-    name: 'Sales of Product Income',
-    value: '79',
-  },
-  AssetAccountRef: {
-    name: 'Inventory Asset',
-    value: '81',
-  },
-  ExpenseAccountRef: {
-    name: 'Cost of Goods Sold',
-    value: '80',
-  },
-};
-
-oauthClient
-  .makeApiCall({
-    url: `${url}v3/company/${companyID}/customer?minorversion=73`,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body),
-  })
-  .then(function (response) {
-    console.log('The API response is  : ' + response);
-    res.json(response);
-  })
-  .catch(function (e) {
-    console.log('The error is ' + JSON.stringify(e));
-  });
+  oauthClient
+    .makeApiCall({ url: `${url}v3/company/${companyID}/companyinfo/${companyID}` })
+    .then(function (authResponse) {
+      console.log(`\n The response for API call is :${JSON.stringify(authResponse.json)}`);
+      res.send(authResponse.json);
+    })
+    .catch(function (e) {
+      console.error(e);
+    });
 });
+
+
 
 /* POST to execute */
 router.post('/execute', async function(req, res, next) {
