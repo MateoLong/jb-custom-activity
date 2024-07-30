aebd3434520da3b180bc740baaa4cad5727b2869
Mateo Long <mlongg@deloitte.com>
2024-07-29 22:22:17 -0300

    cors()

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
